8373118b1f67ca35fbf761ab8202606860f25144
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-01 11:00:00 +0530

    feat: add department and role response DTOs

---
+++ TeacherService/src/main/java/com/UMS/TeacherService/dto/DepartmentResponseDTO.java
@@ -0,0 +1,11 @@
+package com.UMS.TeacherService.dto;
+
+import lombok.Data;
+
+@Data
+public class DepartmentResponseDTO {
+
+
+    private Long departmentId;
+    private String departmentName;
+}
---
+++ TeacherService/src/main/java/com/UMS/TeacherService/dto/RoleResponseDTO.java
@@ -0,0 +1,10 @@
+package com.UMS.TeacherService.dto;
+
+import lombok.Data;
+
+@Data
+public class RoleResponseDTO {
+
+    private Long roleId;
+    private String roleName;
+}
