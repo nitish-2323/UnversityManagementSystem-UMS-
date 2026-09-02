ea3eb62f532bb9428e6df60c4d968182776d21f3
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-02 19:47:53 +0530

    feat: add department and teacher service communication

--- CourseService/src/main/java/com/UMS/CourseService/client/DepartmentClient.java
+++ CourseService/src/main/java/com/UMS/CourseService/client/DepartmentClient.java
@@ -1,4 +1,36 @@
 package com.UMS.CourseService.client;
 
+import com.UMS.CourseService.dto.DepartmentResponseDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
 public class DepartmentClient {
+
+    private final RestTemplate restTemplate;
+
+    @CircuitBreaker(
+            name = "DepartmentService",
+            fallbackMethod = "departmentServiceFallback"
+    )
+    public DepartmentResponseDTO getDepartmentById(Long departmentId) {
+
+        return restTemplate.getForObject(
+                        "http://DepartmentService/departments/" + departmentId,
+                        DepartmentResponseDTO.class);
+    }
+
+    public DepartmentResponseDTO departmentServiceFallback(
+            Long departmentId, Exception ex) {
+
+        DepartmentResponseDTO response = new DepartmentResponseDTO();
+
+        response.setDepartmentId(departmentId);
+        response.setDepartmentName("Department Service Unavailable");
+
+        return response;
+    }
 }
--- CourseService/src/main/java/com/UMS/CourseService/client/TeacherClient.java
+++ CourseService/src/main/java/com/UMS/CourseService/client/TeacherClient.java
@@ -1,4 +1,43 @@
 package com.UMS.CourseService.client;
 
+import com.UMS.CourseService.dto.TeacherResponseDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
 public class TeacherClient {
+    private final RestTemplate restTemplate;
+
+    @CircuitBreaker(
+            name = "TeacherService",
+            fallbackMethod = "teacherServiceFallback"
+    )
+    public TeacherResponseDTO getTeacherById(Long teacherId) {
+        return restTemplate.getForObject(
+                        "http://TeacherService/teachers/" + teacherId,
+                TeacherResponseDTO.class);
+    }
+
+    public TeacherResponseDTO teacherServiceFallback(
+            Long teacherId, Exception ex) {
+        System.out.println("========== TEACHER SERVICE ERROR ==========");
+        System.out.println("Teacher ID: " + teacherId);
+        System.out.println("Exception: " + ex.getClass().getName());
+        System.out.println("Message: " + ex.getMessage());
+        ex.printStackTrace();
+        System.out.println("============================================");
+
+        TeacherResponseDTO response = new TeacherResponseDTO();
+
+        response.setTeacherId(teacherId);
+        response.setFirstName("Teacher Service Unavailable");
+        response.setLastName("");
+        response.setEmail("");
+        response.setSpecialization("");
+
+        return response;
+    }
 }
