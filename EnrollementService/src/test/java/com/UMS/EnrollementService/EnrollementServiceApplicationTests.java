402fa4de8e2c2eb214fce8b6f3962f6e705512b4
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-03 19:37:55 +0530

    chore: add EnrollmentService project support files

---
+++ EnrollementService/src/test/java/com/UMS/EnrollementService/EnrollementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UMS.EnrollementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
