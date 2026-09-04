7ef58daf6220b0dfa3bf59f914307b75a0163480
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-04 12:36:07 +0530

    feat: add attendance exception handling

---
+++ AttendanceService/src/main/java/com/UMS/AttendanceService/execption/AttendanceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.UMS.AttendanceService.execption;
+
+public class AttendanceNotFoundException extends RuntimeException {
+    public AttendanceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ AttendanceService/src/main/java/com/UMS/AttendanceService/execption/GlobalExecptionHandler.java
@@ -0,0 +1,24 @@
+package com.UMS.AttendanceService.execption;
+
+import com.UMS.AttendanceService.dto.ExecptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExecptionHandler {
+    @ExceptionHandler(AttendanceNotFoundException.class)
+    public ResponseEntity<ExecptionDTO> execptionDepartmentIdNotFound(Exception e, WebRequest webRequest){
+        ExecptionDTO obj = new ExecptionDTO();
+        obj.setStatus(HttpStatus.NOT_FOUND.value());
+        obj.setMsg(e.getMessage());
+        obj.setPath(webRequest.getDescription(false));
+        obj.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(obj,HttpStatus.NOT_FOUND);
+    }
+
+}
