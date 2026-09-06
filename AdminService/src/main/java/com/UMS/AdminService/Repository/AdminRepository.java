9241be986388698dae9c43001c2535c4064d2775
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-06 19:02:20 +0530

    feat: add Admin repository

---
+++ AdminService/src/main/java/com/UMS/AdminService/Repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.UMS.AdminService.Repository;
+
+import com.UMS.AdminService.Entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+
+}
