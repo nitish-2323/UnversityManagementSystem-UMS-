5121dbea737b2c961c4b1c4c21ad2ac0a0c02b02
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-06 19:01:02 +0530

    feat: initialize AdminService project

---
+++ AdminService/src/main/java/com/UMS/AdminService/AdminServiceApplication.java
@@ -0,0 +1,16 @@
+package com.UMS.AdminService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+
+}
+
+
+
