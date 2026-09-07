cb2b3a26729e5ad5230e4c8dfe2cdee0c9feb51d
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-07 19:44:46 +0530

    Add result request and response DTOs

--- ExaminationService/src/main/java/com/UMS/ExaminationService/dto/ResultRequestDTO.java
+++ ExaminationService/src/main/java/com/UMS/ExaminationService/dto/ResultRequestDTO.java
@@ -1,4 +1,12 @@
 package com.UMS.ExaminationService.dto;
 
+import lombok.Data;
+
+@Data
 public class ResultRequestDTO {
+    private Long examId;
+    private Long studentId;
+    private Double marksObtained;
+    private String grade;
+    private String status;
 }
--- ExaminationService/src/main/java/com/UMS/ExaminationService/dto/ResultResponseDTO.java
+++ ExaminationService/src/main/java/com/UMS/ExaminationService/dto/ResultResponseDTO.java
@@ -1,4 +1,15 @@
 package com.UMS.ExaminationService.dto;
 
+import lombok.Data;
+
+@Data
 public class ResultResponseDTO {
+    private Long resultId;
+    private Long examId;
+    private Long studentId;
+    private Double marksObtained;
+    private String grade;
+    private String status;
+    private StudentResponseDTO student;
+
 }
