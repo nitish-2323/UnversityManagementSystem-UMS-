8577881d6c60257159b86952f0195c7571de1c28
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-07 19:46:35 +0530

    Add global exception handling

--- ExaminationService/src/main/java/com/UMS/ExaminationService/dto/ExceptionDTO.java
+++ ExaminationService/src/main/java/com/UMS/ExaminationService/dto/ExceptionDTO.java
@@ -1,5 +1,12 @@
 package com.UMS.ExaminationService.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
 public class ExceptionDTO {
-    
+    private int status;
+    private String msg;
+    private LocalDateTime dateTime;
+    private String path;
 }
--- ExaminationService/src/main/java/com/UMS/ExaminationService/execption/GlobalExecptionHandler.java
+++ ExaminationService/src/main/java/com/UMS/ExaminationService/execption/GlobalExecptionHandler.java
@@ -1,4 +1,23 @@
 package com.UMS.ExaminationService.execption;
 
+
+import com.UMS.ExaminationService.dto.ExceptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+@RestControllerAdvice
 public class GlobalExecptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionDTO> execptionDepartmentIdNotFound(Exception e, WebRequest webRequest){
+        ExceptionDTO obj = new ExceptionDTO();
+        obj.setStatus(HttpStatus.NOT_FOUND.value());
+        obj.setMsg(e.getMessage());
+        obj.setPath(webRequest.getDescription(false));
+        obj.setDateTime(LocalDateTime.now());
+        return new ResponseEntity<>(obj,HttpStatus.NOT_FOUND);
+    }
 }
