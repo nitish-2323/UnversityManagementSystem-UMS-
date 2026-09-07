4f51672c0ec5fcb12fd92a438939ee40c61dd5b9
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-07 19:45:46 +0530

    Add examination result entity

--- ExaminationService/src/main/java/com/UMS/ExaminationService/entity/Result.java
+++ ExaminationService/src/main/java/com/UMS/ExaminationService/entity/Result.java
@@ -1,4 +1,24 @@
 package com.UMS.ExaminationService.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "results")
+@Data
 public class Result {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long resultId;
+
+    private Long examId;
+
+    private Long studentId;
+
+    private Double marksObtained;
+
+    private String grade;
+
+    private String status;
 }
