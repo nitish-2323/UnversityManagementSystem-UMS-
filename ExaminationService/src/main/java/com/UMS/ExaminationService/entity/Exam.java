8559846a07856d825cef8fd75bfb3359df0e57e8
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-07 19:45:36 +0530

    Add examination entity

--- ExaminationService/src/main/java/com/UMS/ExaminationService/entity/Exam.java
+++ ExaminationService/src/main/java/com/UMS/ExaminationService/entity/Exam.java
@@ -1,4 +1,35 @@
 package com.UMS.ExaminationService.entity;
 
-public class Examination {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "exams")
+@Data
+public class Exam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long examId;
+
+    private Long courseId;
+
+    private Long teacherId;
+
+    private String examType;
+
+    private LocalDate examDate;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+    @Column(nullable = false, unique = true)
+    private String roomNumber;
+
+    private Integer totalMarks;
+
+    private String status;
 }
