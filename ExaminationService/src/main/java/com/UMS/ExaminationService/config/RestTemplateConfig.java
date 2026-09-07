6bdc150f7675cc2443e676a98ecdc89664a19f37
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-07 19:44:31 +0530

    Configure RestTemplate for service communication

--- ExaminationService/src/main/java/com/UMS/ExaminationService/config/RestTemplateConfig.java
+++ ExaminationService/src/main/java/com/UMS/ExaminationService/config/RestTemplateConfig.java
@@ -1,4 +1,15 @@
 package com.UMS.ExaminationService.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class RestTemplateConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 }
