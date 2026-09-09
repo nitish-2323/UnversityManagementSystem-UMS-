14c8d8b75d5c37f289b2e63175a813f5f2032b3e
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-09 19:38:43 +0530

    Create notification repository

---
+++ NotificationService/src/main/java/com/UMS/NotificationService/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package com.UMS.NotificationService.repository;
+import com.UMS.NotificationService.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public interface NotificationRepository extends JpaRepository<Notification,Long> {
+
+
+    List<Notification> findByStudentId(Long studentId);
+
+    List<Notification> findByStudentIdAndReadAtIsNull(Long studentId);
+}
