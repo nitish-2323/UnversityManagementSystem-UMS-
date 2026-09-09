f2908d2da5e53f36c7f923a6e4a42cecff59d28e
Nitish Thakur <nitishrajput2604@gmail.com>
2026-09-09 19:38:20 +0530

    Add notification status and type enums

---
+++ NotificationService/src/main/java/com/UMS/NotificationService/entity/NotificationStatus.java
@@ -0,0 +1,7 @@
+package com.UMS.NotificationService.entity;
+
+public enum NotificationStatus {
+    PENDING,
+    SENT,
+    FAILED
+}
---
+++ NotificationService/src/main/java/com/UMS/NotificationService/entity/NotificationType.java
@@ -0,0 +1,8 @@
+package com.UMS.NotificationService.entity;
+
+public enum NotificationType {
+
+    EMAIL,
+    SMS,
+    IN_APP
+}
